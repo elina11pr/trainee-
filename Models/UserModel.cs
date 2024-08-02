6590cd6b43f4d9166621474083e6f209fe62a144
elina11pr <119628554+elina11pr@users.noreply.github.com>
2024-08-02 15:12:23 +0300

    Create UserModel

---
+++ Models/UserModel.cs
@@ -0,0 +1,11 @@
+namespace UserData.Models;
+
+public class UserModel
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public int Age { get; set; }
+    public required string Email { get; set; }
+
+    
+};
